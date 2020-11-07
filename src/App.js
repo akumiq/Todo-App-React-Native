1aa6a71bc632338f30c95da3d4c15dcc466160fe
Miqdad <kun.miqdad23@gmail.com>
2020-11-07 13:26:32 +0700

    add and setup package react navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Dimensions, BackHandler} from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 
 import DashboardScreen from '../src/screens/DashboardScreen';
 
+const Stack = createStackNavigator();
+
 const App = () => {
-  return <DashboardScreen />;
+  const entireScreenWidth = Dimensions.get('window').width;
+  EStyleSheet.build({$rem: entireScreenWidth / 320});
+
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      () => {
+        BackHandler.exitApp();
+      },
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="DashboardScreen"
+        screenOptions={{
+          headerShown: false,
+          ...TransitionPresets.SlideFromRightIOS,
+        }}>
+        <Stack.Screen name="DashboardScreen" component={DashboardScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
