659941941626d2489d5da3ece93467d01519c386
Miqdad <kun.miqdad23@gmail.com>
2020-12-11 09:12:58 +0700

    add icon edit and delete

--- src/components/molekul/TodoListComponent.js
+++ src/components/molekul/TodoListComponent.js
@@ -1,23 +1,46 @@
 import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import IconEdit from 'react-native-vector-icons/FontAwesome';
+import IconTrash from 'react-native-vector-icons/FontAwesome5';
 
 const TodoList = (props) => {
   return (
-    <TouchableOpacity onPress={props.onPress}>
-      <Text style={styles.item}>{props.titleList}</Text>
-    </TouchableOpacity>
+    <View style={styles.wrapperList}>
+      <Text>{props.titleList}</Text>
+
+      <View style={styles.wrapperIcon}>
+        <TouchableOpacity>
+          <IconEdit name="edit" size={22} />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={props.onPress} style={styles.icon}>
+          <IconTrash name="trash" size={22} />
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
 export default TodoList;
 
 const styles = EStyleSheet.create({
-  item: {
-    padding: '14rem',
+  wrapperList: {
     marginTop: '10rem',
+    paddingVertical: '10rem',
+    paddingHorizontal: '10rem',
     borderWidth: 1,
     borderRadius: '10rem',
     borderColor: '#bbb',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  wrapperIcon: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginLeft: '20rem',
   },
 });
