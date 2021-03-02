e9f0cc2d3df205aecb0e01bf900a63a4f069d5bb
Miqdad <kun.miqdad23@gmail.com>
2021-03-02 21:33:37 +0700

    change main file to run apk

--- index.js
+++ index.js
@@ -2,8 +2,38 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import AwesomeIcon from 'react-native-vector-icons';
+import { Provider } from 'react-redux';
 import App from './src/App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import store from './src/redux/store/store';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+  ...DefaultTheme,
+  roundness: 8,
+  dark: true,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#FF9F1C',
+    accent: '#fff',
+  },
+};
+
+export default function Main() {
+  return (
+    <Provider store={store}>
+      <PaperProvider
+        settings={{
+          icon: (props) => <AwesomeIcon {...props} />,
+        }}
+        theme={theme}>
+        <App />
+      </PaperProvider>
+    </Provider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
